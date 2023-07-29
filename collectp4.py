4672dbf6a7a17c73165c648feb54458ed7c2c0a3
daniel54ad <daniel54ad@gmail.com>
2023-07-29 23:14:33 +0800

    add files

---
+++ collectp4.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+#coding=UTF-8
+
+"""
+TCP Client to collect P4 rate-show
+"""
+
+import socket
+import time
+from io import StringIO
+import io
+host = "192.168.13.103"
+port = 9999
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((host,port))
+time.sleep(0.05)
+res = client.recv(32768)
+client.send("ucli\n".encode())
+time.sleep(0.05)
+res = client.recv(32768)
+client.send("rate-period 1\n".encode())
+rx = {}
+tx = {}
+times = 0
+while times < 500000:
+    client.send("rate-show\n".encode())
+    time.sleep(1)
+    res = client.recv(32768)
+    title = True
+    buf = io.StringIO(res.decode("utf-8") )
+    print(buf)
+    # buf = StringIO.StringIO(res)
+    for line in buf.readlines():
+        c = line.split("|")
+        if len(c) == 12 and title is True:
+            title = False
+        elif len(c) == 12:
+            if c[2] not in rx:
+                rx[c[2]] = []
+                tx[c[2]] = []
+            rx[c[2]].append(c[8])
+            tx[c[2]].append(c[9])
+            rate = c[2] + " " + c[8] + " " + c[9] + ":" + str(times)
+            print(rate)
+    print("\n")
+    times += 1
+
+    for key,val in rx.items():
+        f = open("data/rx-"+key, "w")
+        f.write(str(val).strip("[]").replace(',','\n').replace(' ','').replace('\'',''))
+        f.close()
+
+    for key,val in tx.items():
+        f = open("data/tx-"+key, "w")
+        f.write(str(val).strip("[]").replace(',','\n').replace(' ','').replace('\'',''))
+        f.close()
+
+print(rx)
+print(tx)
+
+for key,val in rx.items():
+    f = open("data/rx-"+key, "w")
+    f.write(str(val).strip("[]").replace(',','\n').replace(' ','').replace('\'',''))
+    f.close()
+
+for key,val in tx.items():
+    f = open("data/tx-"+key, "w")
+    f.write(str(val).strip("[]").replace(',','\n').replace(' ','').replace('\'',''))
+    f.close()
+
